1f80c126c952e1fa0214dd5bc43b7cde5215376d
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2023-01-25 11:59:17 +0100

    Create ttt.sh

---
+++ ttt.sh
@@ -0,0 +1,9 @@
+i=1
+while [ $i -le 100 ]
+do
+
+        echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+        i=$(($i+1))
+        sleep 0.2
+done
