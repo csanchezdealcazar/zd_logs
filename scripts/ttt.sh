faa14dc4823c0abc8f53336e1f7f4bff0f1436ef
csanchezdealcazar <107619515+csanchezdealcazar@users.noreply.github.com>
2023-01-25 12:50:03 +0100

    Update ttt.sh

--- scripts/ttt.sh
+++ scripts/ttt.sh
@@ -1,4 +1,5 @@
 !#/bin/bash
+set -x
 i=1
 while [ $i -le 100 ]
 do
@@ -6,5 +7,5 @@ do
         echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
         i=$(($i+1))
-        sleep 0.2
+       
 done
